d3eca89310ce104d38b855d29779aad6da45608d
Mohd Yunus <112767616+Yunuslala@users.noreply.github.com>
2023-10-06 10:38:06 +0530

    updated read me file

--- backend/routers/User.Router.js
+++ backend/routers/User.Router.js
@@ -3,14 +3,15 @@ const UserRouter=require("express").Router();
 const {UserRegister,UserLogIn}=require('../controllers/User.Controller');
 const { body, validationResult } = require("express-validator");
 
+
 UserRouter.post('/Register',
-body("email").trim().isEmail(),
-body("password").trim().isLength({ min: 6 }),
-body("mobileNumber").trim().isLength({ min: 10 }),
+body("email").trim().isEmail().withMessage("email should be in eamil format"),
+body("password").trim().isLength({ min: 6 }).withMessage("password must be minimum 6 letters"),
+body("mobileNumber").trim().isLength({ min: 10 }).withMessage("mobilenumber must be in a format minimum 10 letters"),
 UserRegister);
 UserRouter.post('/login',
 body("email").trim().isEmail(),
-body("password").trim().isLength({ min: 6 }),
+body("password").trim().isLength({ min: 6 }).withMessage("password must be minimum 6 letters"),
 UserLogIn);
 
 module.exports={
